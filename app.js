c92b98417601d43c7a316c08866a52cb7593d2e2
walidMohamed747 <84786097+walidMohamed747@users.noreply.github.com>
2023-01-09 11:15:17 +0200

    a

--- app.js
+++ app.js
@@ -12,49 +12,48 @@ const swaggerDocument = require('./swagger.json');
 require("dotenv").config();
 const port = process.env.PORT;
 const fileStorage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images');
-  },
-  filename: (req, file, cb) => {
-    cb(null,  file.originalname);
-  }
+    destination: (req, file, cb) => {
+        cb(null, 'images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
 });
-
 const fileFilter = (req, file, cb) => {
     if (
-      file.mimetype === 'image/png' ||
-      file.mimetype === 'image/jpg' ||
-      file.mimetype === 'image/jpeg'
-      ) {
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
         cb(null, true);
-      } else {
+    } else {
         cb(null, false);
-      }
-    };
-    
-    // app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
-   // const specs = swaggerJsDoc(options);
-    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    app.use(bodyParser.json()); // application/json
-  app.use(
+    }
+};
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+// const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.json()); // application/json
+app.use(
     multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
-  );
-  app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use((req , res , next) => {
-    res.setHeader("Access-Control-Allow-Origin" , "*")
-    res.setHeader("Access-Control-Allow-Methods" , "GET , POST , PUT , PATCH , DELETE")
-    res.setHeader("Access-Control-Allow-Headers" , "Content-Type , Authorization") 
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET , POST , PUT , PATCH , DELETE")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type , Authorization")
 
     next()
 })
-app.use('/tasks' , tasksRoute)
-app.use('/auth' , authRoute)
+app.use('/tasks', tasksRoute)
+app.use('/auth', authRoute)
 
 app.use((error, req, res, next) => {
     const status = error.statusCode || 500;
     const message = error.message;
     res.status(status).json({ message: message });
-  });
+});
 DBConcction(() => {
     app.listen(port)
 })
--- util/database.js
+++ util/database.js
@@ -1,11 +1,20 @@
 const mongoose = require("mongoose");
 
-const DBConcction = callback => { mongoose.connect(process.env.MONGO_URL).then( ()=> {
-         console.log("DB Connected!!")
-         callback()
+const DBConcction = callback => {
+    mongoose.connect('mongodb+srv://walid:Walid@1234@cluster0.rcqbbao.mongodb.net/?retryWrites=true&w=majority').then(() => {
+        console.log("DB Connected!!")
+        callback()
     }).catch(err => {
         console.log(err);
     })
 }
 
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
 exports.DBConcction = DBConcction;
